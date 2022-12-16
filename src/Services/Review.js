9700cd0cecc0614b630ff8358262a3e4182421ef
aad3sh <mallya.aa@northeastern.edu>
2022-12-16 10:06:06 -0500

    connecting to heroku server

--- src/Services/Review.js
+++ src/Services/Review.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const API_URL = "https://tuiter-node-server-app-rishi.herokuapp.com/api";
-const API_URL = "http://localhost:4300/api";
+const API_URL = "https://tuiter-node-server-app-rishi.herokuapp.com/api";
 
 const CREATE_REVIEW_URL = `${API_URL}/add-review`;
 const DELETE_REVIEW_URL = `${API_URL}/remove-review`;
